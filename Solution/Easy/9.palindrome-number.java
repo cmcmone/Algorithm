64c93b2e9af59008efd65dd3ede6a92b2a4a8a52
Shengrui <cmcmone@gmail.com>
2020-05-23 17:34:11 -0400

    solve problem 9

--- Solution/Easy/9.palindrome-number.java
+++ Solution/Easy/9.palindrome-number.java
@@ -8,7 +8,21 @@
 // @lc code=start
 class Solution {
     public boolean isPalindrome(int x) {
-        
+        int res = 0, org = x;
+        if (x == 0) {
+            return true;
+        } else if (x < 0 || (x % 10) == 0) {
+            return false;
+        } else {
+            while (x != 0) {
+                res = res * 10 + x % 10;
+                x /= 10;
+            }
+            if (res == org) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 // @lc code=end
