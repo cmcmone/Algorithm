79564dbc13526dcb4018c078a06d78761020eb26
Shengrui <cmcmone@gmail.com>
2020-05-27 22:22:35 -0400

    add approach 2 to solve problem 28

--- Leetcode/Solution/Easy/28.implement-str-str.java
+++ Leetcode/Solution/Easy/28.implement-str-str.java
@@ -6,9 +6,13 @@
  * Problem Description:
  * - see [28] Implement strStr()
  * - https://leetcode.com/problems/implement-strstr/
+ * 
+ * Approach 1: Brute Force
+ * Approach 2: Boyer Moore Algorithm(Bad Character Heuristic)
  */
 
 // @lc code=start
+// Approach 1: Brute Force
 class Solution {
     public int strStr(String haystack, String needle) {
         if (needle.isEmpty()) return 0;
@@ -26,4 +30,39 @@ class Solution {
         return -1;
     }
 }
+/*
+//Approach 2: Boyer Moore Algorithm(Bad Character Heuristic)
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty())
+            return 0;
+        if (haystack.isEmpty())
+            return -1;
+
+        var badchar = new HashMap<Character, Integer>(needle.length());
+        badCharHeuristic(needle, badchar);
+
+        int pos = 0, len;
+        while (pos <= haystack.length() - needle.length()) {
+            for (len = needle.length() - 1; len >= 0 && haystack.charAt(pos + len) == needle.charAt(len); len--) {
+                if (len <= 0) {
+                    return pos;
+                }
+            }
+            pos += Math.max(1, len - getValue(badchar, haystack.charAt(pos + len)));
+        }
+        return -1;
+    }
+
+    private int getValue(HashMap<Character, Integer> badchar, char c) {
+        return badchar.get(c) == null ? -1 : badchar.get(c);
+    }
+
+    private void badCharHeuristic(String needle, HashMap<Character, Integer> badchar) {
+        for (int i = 0; i < needle.length(); i++) {
+            badchar.put(needle.charAt(i), i);
+        }
+    }
+}
+*/
 // @lc code=end
