bfd5f9156777c0fb926dffc55c1cdd46da5ac4b6
Shengrui <cmcmone@gmail.com>
2020-05-30 22:12:18 -0400

    add Approach 2: divide and conquer

--- Leetcode/Solution/Easy/53.maximum-subarray.java
+++ Leetcode/Solution/Easy/53.maximum-subarray.java
@@ -12,6 +12,8 @@
  */
 
 // @lc code=start
+/*
+//Approach 1: iterative
 class Solution {
     public int maxSubArray(int[] nums) {
         int sum = nums[0];
@@ -21,6 +23,44 @@ class Solution {
             max = Math.max(sum, max);
         }
         return max;
+    }
+}
+*/
+//Approach 2: divide and conquer
+class Solution {
+    public int maxSubArray(int[] nums) {
+        return maxSub(nums, 0, nums.length - 1);
+    }
+
+    private int maxSub(int[] nums, int left, int right) {
+        //base case
+        if (left == right) return nums[left];
+        //divide
+        int mid = left + (right - left) / 2;
+        //conquer
+        int leftSum = maxSub(nums, left, mid);
+        int rightSum = maxSub(nums, mid + 1, right);
+        //combine
+        int crossSum = cross(nums, left, right);
+        
+        return Math.max(crossSum, Math.max(leftSum, rightSum));
+    }
+
+    private int cross(int[] nums, int left, int right) {
+        int mid = left + (right - left) / 2;
+        //left
+        int leftSum = 0, leftMax = Integer.MIN_VALUE;
+        for (int i = mid; i >= left; --i) {
+            leftSum += nums[i];
+            leftMax = Math.max(leftMax, leftSum);
+        }
+        //right
+        int rightSum = 0, rightMax = Integer.MIN_VALUE;
+        for (int i = mid + 1; i <= right; ++i) {
+            rightSum += nums[i];
+            rightMax = Math.max(rightMax, rightSum);
+        }
+        return leftMax + rightMax;
     }
 }
 // @lc code=end
