cd1e7bab081d1564dc4c582fcf2af50248afd067
Shengrui <cmcmone@gmail.com>
2020-05-31 22:07:54 -0400

    solve problem 58

--- Leetcode/Solution/Easy/58.length-of-last-word.java
+++ Leetcode/Solution/Easy/58.length-of-last-word.java
@@ -11,7 +11,22 @@
 // @lc code=start
 class Solution {
     public int lengthOfLastWord(String s) {
-        
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int len = s.length() - 1;
+        while (len >= 0 && s.charAt(len) == ' ') {
+            len--;
+        }
+        for (int i = len; i >= 0; --i) {
+            if (s.charAt(i) == ' ') {
+                return len - i;
+            }
+            if (i == 0) {
+                return ++len;
+            }
+        }
+        return 0;
     }
 }
 // @lc code=end
