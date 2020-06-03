e74a13504032629b8260d7db4455c45cf0a6fe03
Shengrui <cmcmone@gmail.com>
2020-06-03 18:17:11 -0400

    solve the problem 107

---
+++ Leetcode/Solution/Easy/107.binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=107 lang=java
+ * @version 1.00 June 3, 2020
+ * @author Ray Chen
+ * 
+ * Problem Description:
+ * - [107] Binary Tree Level Order Traversal II
+ * - https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+ * 
+ * Approach 1: recursively 
+ * Approach 2: iteratively
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        
+    }
+}
+// @lc code=end
+
