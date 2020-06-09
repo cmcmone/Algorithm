1a2a3c83a405b053f3d991b05cd454eaa375cc09
Shengrui <cmcmone@gmail.com>
2020-06-08 21:40:38 -0400

    format code use spaces

--- Leetcode/Solution/Easy/1.two-sum.java
+++ Leetcode/Solution/Easy/1.two-sum.java
@@ -10,20 +10,20 @@
 
 // @lc code=start
 class Solution {
-	public int[] twoSum(int[] nums, int target) {
-		int[] res = new int[2];
-		var map = new HashMap<Integer, Integer>();
-		int remainder;
-		for (int i = 0; i < nums.length; i++) {
-			remainder = target - nums[i];
-			if (map.containsKey(remainder)) {
-				res[0] = map.get(remainder);
-				res[1] = i;
-				break;
-			}
-			map.put(nums[i], i);
-		}
-		return res;
-	}
+    public int[] twoSum(int[] nums, int target) {
+        int[] ret = new int[2];
+        var map = new HashMap<Integer, Integer>();
+        int remainder;
+        for (int i = 0; i < nums.length; i++) {
+            remainder = target - nums[i];
+            if (map.containsKey(remainder)) {
+                ret[0] = map.get(remainder);
+                ret[1] = i;
+                break;
+            }
+            map.put(nums[i], i);
+        }
+        return ret;
+    }
 }
 // @lc code=end
